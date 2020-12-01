cce91a572d51e1df4bd37ff1f5e399632e006799
leks <kurbakov.av@gmail.com>
2020-12-01 14:05:50 +0300

    define: TenantRepository

---
+++ src/main/java/com/example/bookshelf/repository/TenantRepository.java
@@ -0,0 +1,14 @@
+package com.example.bookshelf.repository;
+
+import com.example.bookshelf.domain.app.Tenant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TenantRepository extends JpaRepository<Tenant, Long> {
+    Optional<Tenant> findByName(String name);
+
+    Optional<Tenant> findBySchema(String schema);
+}
