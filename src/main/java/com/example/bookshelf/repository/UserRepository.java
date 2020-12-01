cd636b524c18c16ca6999b824e3b2f7d9ccc31f9
leks <kurbakov.av@gmail.com>
2020-12-01 14:06:10 +0300

    define: UserRepository

---
+++ src/main/java/com/example/bookshelf/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.bookshelf.repository;
+
+import com.example.bookshelf.domain.app.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Optional<User> findByUsername(String username);
+}
