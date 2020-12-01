9da8e99b5d9081aa8ea22748abec21179f55fbf2
leks <kurbakov.av@gmail.com>
2020-12-01 14:05:28 +0300

    define: GenreRepository

---
+++ src/main/java/com/example/bookshelf/repository/GenreRepository.java
@@ -0,0 +1,9 @@
+package com.example.bookshelf.repository;
+
+import com.example.bookshelf.domain.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+}
