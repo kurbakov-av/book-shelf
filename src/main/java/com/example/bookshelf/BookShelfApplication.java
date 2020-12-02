8842877eb47cc9aed9dd3813dfd3aad6c64c8eaa
leks <kurbakov.av@gmail.com>
2020-12-02 23:18:11 +0300

    add: BookShelfApplication

---
+++ src/main/java/com/example/bookshelf/BookShelfApplication.java
@@ -0,0 +1,11 @@
+package com.example.bookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShelfApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookShelfApplication.class, args);
+    }
+}
